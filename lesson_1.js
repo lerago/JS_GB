ac72779168760ccb3cd167743138c690341b9670
lerago <valeriya.golenkina@yandex.ru>
2022-09-19 10:45:02 +0300

    added new data in lesson 2

--- lesson_1.js
+++ lesson_1.js
@@ -7,13 +7,37 @@ const goods = [
    { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = ({title = " ", price = 0}) => {
-   return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
+class GoodsItem {
+   constructor({ title = " ", price = 0 }) {
+      this.title = title;
+      this.price = price;
+   }
 
-const renderGoodsList = (list = []) => {
-   let goodsList = list.map(item => renderGoodsItem(item)).join('');
-   document.querySelector('.goods-list').innerHTML = goodsList;
+   render() {
+      return `
+      <div class="goods-item">
+         <h3>${this.title}</h3>
+         <p>${this.price}</p>
+      </div>
+      `
+   }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+   list = [];
+   fetchGoods() {
+      this.list = goods;
+   }
+   render() {
+      const resultList = this.list.map(item => {
+         const goodsItem = new GoodsItem(item);
+         return goodsItem.render();
+      });
+      document.querySelector('.goods-list').innerHTML = resultList.join('');
+   }
+}
+
+
+const goodsList = new GoodsList ();
+goodsList.fetchGoods();
+goodsList.render();
