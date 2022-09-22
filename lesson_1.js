d252aa0e846ff9779f42efed410c90e65dcdc65c
lerago <valeriya.golenkina@yandex.ru>
2022-09-22 15:04:58 +0300

    added new date in home work 3

--- lesson_1.js
+++ lesson_1.js
@@ -1,22 +1,32 @@
 'use strict';
 
-const goods = [
-   { title: 'Shirt', price: 150 },
-   { title: 'Socks', price: 50 },
-   { title: 'Jacket', price: 350 },
-   { title: 'Shoes', price: 250 },
-];
+const URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+const GOODS = "/catalogData.json";
+
+const url = `${URL}${GOODS}`;
+
+function service(url) {
+   return new Promise((resolve) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.onload = () => {
+         resolve(JSON.parse(xhr.response))
+      };
+      xhr.send();
+   })
+}
+
 
 class GoodsItem {
-   constructor({ title = " ", price = 0 }) {
-      this.title = title;
+   constructor({ product_name = " ", price = 0 }) {
+      this.product_name = product_name;
       this.price = price;
    }
 
    render() {
       return `
       <div class="goods-item">
-         <h3>${this.title}</h3>
+         <h3>${this.product_name}</h3>
          <p>${this.price}</p>
       </div>
       `
@@ -24,10 +34,15 @@ class GoodsItem {
 }
 
 class GoodsList {
-   list = [];
+   items = [];
    fetchGoods() {
-      this.items = goods;
+      return new Promise((resolve) => {
+      service(url).then ((data) => {
+         this.items = data;
+         resolve()
+      })});
    }
+
    render() {
       const resultList = this.items.map(item => {
          const goodsItem = new GoodsItem(item);
@@ -36,16 +51,16 @@ class GoodsList {
       document.querySelector('.goods-list').innerHTML = resultList.join('');
    }
 
-   sumPrice () {
-      return this.items.reduce (function (acc, item) {
+   sumPrice() {
+      return this.items.reduce(function (acc, item) {
          return acc + item.price
       }, 0)
    }
 }
 
 
-const goodsList = new GoodsList ();
-goodsList.fetchGoods();
-goodsList.render();
-const result = goodsList.sumPrice();
-console.log(result);
+const goodsList = new GoodsList();
+goodsList.fetchGoods().then(() => {
+   goodsList.render();
+});
+
